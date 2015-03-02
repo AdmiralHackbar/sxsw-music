f780ddd3a51976749a86630ac2efb9c0c3670e6e
AdmiralHackbar <the.admiral.hackbar@gmail.com>
2015-03-01 23:48:37 -0600

    Read secret from environmental variable.

--- sxswapp/settings.py
+++ sxswapp/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-z-akeziqy3z!8zs4&8m@_jf#1=s2z*i6evgg55l^i8_-nh0so'
+SECRET_KEY = os.environ['DJANGO_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
