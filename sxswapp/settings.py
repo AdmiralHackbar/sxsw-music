4631cf1c8db19acc85f0bc7c11dc9fc99626a2ef
AdmiralHackbar <the.admiral.hackbar@gmail.com>
2015-03-02 19:30:24 -0600

    setting bug.

--- sxswapp/settings.py
+++ sxswapp/settings.py
@@ -98,8 +98,7 @@ TEMPLATE_DIRS = (
 STATIC_ROOT = "/var/www/sxsw/static/"
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-    STATIC_ROOT
+    os.path.join(BASE_DIR, "static")
 )
 
 
